2c4b5f23ec8c6d8ae76551b85d7019b8cfc8aeb3
szxysdt <szxysdt@outlook.com>
2026-06-04 01:51:57 +0800

    refactor(model_executor): remove unused legacy embedding and norm layers

--- vkwr/model_executor/__init__.py
+++ vkwr/model_executor/__init__.py
@@ -1,7 +1,5 @@
 from vkwr.model_executor.layers.channel_mix import RWKV7ChannelMixDispatcher
-from vkwr.model_executor.layers.embedding import RWKV7Embedding
 from vkwr.model_executor.layers.linear import RWKV7LinearDispatcher
-from vkwr.model_executor.layers.norm import RWKV7AddLayerNorm, RWKV7LayerNorm
 from vkwr.model_executor.layers.path_dispatcher_config import CmixConfig, PathConfig
 from vkwr.model_executor.layers.sampler import RWKV7Sampler
 from vkwr.model_executor.layers.time_mix import RWKV7TimeMixDispatcher
@@ -12,10 +10,7 @@ __all__ = [
     "CmixConfig",
     "PathConfig",
     # Layers
-    "RWKV7AddLayerNorm",
     "RWKV7ChannelMixDispatcher",
-    "RWKV7Embedding",
-    "RWKV7LayerNorm",
     "RWKV7LinearDispatcher",
     "RWKV7Sampler",
     "RWKV7TimeMixDispatcher",
